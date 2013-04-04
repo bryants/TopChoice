ab2e71ec7866823a8b21bcb602aa0f602f42b59e
bryants <bryantsatterfield@gmail.com>
2013-04-04 01:38:58 -0400

    fixing all of the typos, index page now reachable

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,10 +1,10 @@
 class Entry < ActiveRecord::Base
-FORM_OPTIONS = ['ruby', 'javascript', 'python', 'php', 'other']
-  attr_accessible :choice
+  FORM_OPTIONS = ['ruby', 'javascript', 'python', 'php', 'other']
+  #attr_accessible :choice
 
   def self.get_results
-    counts = Entry count({ :group => :choice })
-    FORM_OPTION.map do |o|
+    counts = Entry.count({ :group => :choice })
+    FORM_OPTIONS.map do |o|
     {
       'title' => o,
       'votes' => counts[o]||0
