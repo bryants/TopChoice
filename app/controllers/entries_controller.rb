74d0cb8638e7f8fa475500bd4bab96efccb78f18
bryants <bryantsatterfield@gmail.com>
2013-04-04 04:59:39 -0400

    it works 95%

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,3 +1,8 @@
+require 'pusher'
+Pusher.app_id = '40867'
+Pusher.key = 'fadea4049d9491041f20'
+Pusher.secret = '2ac7778b03d7d2f277af'
+
 class EntriesController < ApplicationController
   def index
     @entries = Entry.get_results
@@ -5,6 +10,7 @@ class EntriesController < ApplicationController
 
   def create
     Entry.create(:choice =>params[:choice])
+    Pusher['app-dev-real_time-test'].trigger('data-changed', Entry.get_results.to_json)
     redirect_to '/'
   end
 end
